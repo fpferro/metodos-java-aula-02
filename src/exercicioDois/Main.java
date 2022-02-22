6660b760dec004df4bf04476808a3ce02a4673a4
fpferro <fabricioferro_@hotmail.com>
2022-02-21 21:00:20 -0300

    Fist commit

---
+++ src/exercicioDois/Main.java
@@ -0,0 +1,16 @@
+package exercicioDois;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Quadrilátero
+		System.out.println("Exercício quadrilátero");
+		Quadrilatero.area(3);
+		Quadrilatero.area(5D, 5D);
+		Quadrilatero.area(7, 8, 9);
+		Quadrilatero.area(5F, 5F);
+
+	}
+
+}
